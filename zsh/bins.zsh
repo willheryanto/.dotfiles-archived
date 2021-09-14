913904727c4bea63382f68f9a5af56b3127309ba
William Heryanto <will.heryanto@gmail.com>
2021-09-14 13:28:16 +0700

    feat: update python bins

--- zsh/bins.zsh
+++ zsh/bins.zsh
@@ -1,3 +1,4 @@
 export PATH=$PATH:$DOTFILES/bin # My own bin
+export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/usr/local/Cellar/llvm/12.0.1/bin # llvm
 export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin # Google Cloud SDK
