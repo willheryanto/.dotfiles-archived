6053c621bc6827608d95eb11544e2da873275bc6
willheryanto <will.heryanto@gmail.com>
2021-12-28 22:23:09 +0700

    refactor: rust tools without formatting capability

--- neovim/.config/nvim/lua/plugin_settings/nvim/rust-tools.lua
+++ neovim/.config/nvim/lua/plugin_settings/nvim/rust-tools.lua
@@ -1,1 +1,10 @@
-require('rust-tools').setup({})
+local helpers = require 'lsp.helpers'
+
+require('rust-tools').setup {
+    server = {
+        cmd = { 'rust-analyzer' },
+        filetypes = { 'rust' },
+        root_dir = require('lspconfig/util').root_pattern('Cargo.toml', 'rust-project.json'),
+        on_attach = helpers.on_attach_navigation,
+    },
+}
