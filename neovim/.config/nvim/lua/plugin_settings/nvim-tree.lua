41a5d1dade514a13eb783376294521642b525f30
William Heryanto <will.heryanto@gmail.com>
2022-07-14 15:39:17 +0700

    chore: nvim-tree config migration

--- neovim/.config/nvim/lua/plugin_settings/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugin_settings/nvim-tree.lua
@@ -1,71 +1,78 @@
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ':~'
-
-g.nvim_tree_window_picker_exclude = {
-    filetype = {
-        'notify',
-        'packer',
-        'qf',
-    },
-    buftype = {
-        'terminal',
-    },
-}
-
-g.nvim_tree_special_files = {
-    ['README.md'] = 1,
-    Makefile = 1,
-    MAKEFILE = 1,
-}
-g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 0,
-}
-
-g.nvim_tree_icons = {
-    default = ' ',
-    symlink = ' ',
-    git = {
-        unstaged = '✗',
-        staged = '✓',
-        unmerged = '',
-        renamed = '➜',
-        untracked = '★',
-        deleted = '',
-        ignored = '◌',
-    },
-    folder = {
-        arrow_open = '',
-        arrow_closed = '',
-        default = '',
-        open = '',
-        empty = '',
-        empty_open = '',
-        symlink = '',
-        symlink_open = '',
-    },
-}
-
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require('nvim-tree').setup {
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+        highlight_git = true,
+        root_folder_modifier = ':~',
+
+        icons = {
+
+            glyphs = {
+                default = ' ',
+                symlink = ' ',
+                git = {
+                    unstaged = '✗',
+                    staged = '✓',
+                    unmerged = '',
+                    renamed = '➜',
+                    untracked = '★',
+                    deleted = '',
+                    ignored = '◌',
+                },
+                folder = {
+                    arrow_open = '',
+                    arrow_closed = '',
+                    default = '',
+                    open = '',
+                    empty = '',
+                    empty_open = '',
+                    symlink = '',
+                    symlink_open = '',
+                },
+            },
+
+            show = {
+                git = true,
+                folder = true,
+                file = true,
+                folder_arrow = false,
+            },
+        },
+
+        special_files = {
+            ['README.md'] = 1,
+            Makefile = 1,
+            MAKEFILE = 1,
+        },
+    },
+
+    actions = {
+        open_file = {
+            window_picker = {
+                exclude = {
+                    filetype = {
+                        'notify',
+                        'packer',
+                        'qf',
+                    },
+                    buftype = {
+                        'terminal',
+                    },
+                },
+            },
+        },
+    },
+
     disable_netrw = true,
     hijack_netrw = false,
     open_on_setup = false,
-    ignore_ft_on_setup = { ".git" },
-    auto_close = false,
+    ignore_ft_on_setup = { '.git' },
     open_on_tab = false,
     hijack_cursor = true,
     update_cwd = false,
-    update_to_buf_dir = {
-        enable = true,
-        auto_open = true,
-    },
     diagnostics = {
         enable = false,
         icons = {
@@ -98,7 +105,6 @@ require('nvim-tree').setup {
         height = 30,
         hide_root_folder = false,
         side = 'left',
-        auto_resize = false,
         mappings = {
             custom_only = false,
             list = {},
