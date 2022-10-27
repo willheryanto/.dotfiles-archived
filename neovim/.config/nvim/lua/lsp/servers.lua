3815d011eecce4ffdd90c200a9aa77d0d7e28746
William Heryanto <will.heryanto@gmail.com>
2022-10-27 10:09:10 +0700

    chore: update deprecated code

--- neovim/.config/nvim/lua/lsp/servers.lua
+++ neovim/.config/nvim/lua/lsp/servers.lua
@@ -1,6 +1,7 @@
 local helpers = require 'lsp.helpers'
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require 'lspconfig'
 
