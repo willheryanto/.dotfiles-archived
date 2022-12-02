14b5e6fbd9d81176ae245bb68a7db47780aea694
William Heryanto <will.heryanto@gmail.com>
2022-12-02 21:37:32 +0700

    chore: remove obsolete

--- neovim/.config/nvim/lua/defaults/commands.lua
+++ neovim/.config/nvim/lua/defaults/commands.lua
@@ -1,23 +1,3 @@
--- Define custom :GBrowse for WSL to work with vim-rhubarb
-
 vim.cmd [[
-function! IsWSL()
-  if has("macunix")
-    return 0
-  elseif has("unix")
-    let lines = readfile("/proc/version")
-    if lines[0] =~ "Microsoft"
-      return 1
-    endif
-  endif
-  return 0
-endfunction
-
-function! MyBrowse(url)
-  execute '!explorer.exe' shellescape(a:url, 1)
-endfunction
-
-if (IsWSL())
-    command! -nargs=1 Browse silent call MyBrowse(<q-args>)
-endif
+command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>, 1)
 ]]
